1a9d75087d823eaafce844387645dd95587ee74d
skydreamer <skydreamer210@gmail.com>
2024-05-15 14:36:12 +0900

    fix: AlbumActivityWebDto 불필요 import 정리

--- src/main/java/org/skydream/blommingprojectprototype/project/adapter/in/web/dto/AlbumActivityWebDto.java
+++ src/main/java/org/skydream/blommingprojectprototype/project/adapter/in/web/dto/AlbumActivityWebDto.java
@@ -1,7 +1,6 @@
 package org.skydream.blommingprojectprototype.project.adapter.in.web.dto;
 
 import lombok.Builder;
-import org.skydream.blommingprojectprototype.project.adapter.out.persistence.entity.AlbumActivityJpaEntity;
 import org.skydream.blommingprojectprototype.project.domain.AlbumActivity;
 
 @Builder
